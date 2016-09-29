6ecc9cc95aceb1755a89adca4a5052c5c47e8c05
Sergio Forés Raga <sergiofores@gmail.com>
2016-09-29 14:37:16 +0200

    Save local changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import './css/Index.css'; 
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+
+
+// import "./scss/main.scss"
+//
+// import React from 'react';
+// import { render } from 'react-dom'
+// import { browserHistory, Router, Route, Link } from 'react-router'
+// import { hashHistory } from 'react-router'
+// import routes from './config/routes'
+//
+// // <Router history={browserHistory}>
+// render(
+//   <Router history={hashHistory}>{routes}</Router>,
+//   document.getElementById('root')
+// )
